ab895ff45b101f752810bba364f820fe552e28c9
Will Dorsey <williamdorseyv@gmail.com>
2018-12-05 06:56:57 +0000

    Initial commit

---
+++ get_data.py
@@ -0,0 +1,41 @@
+import os
+
+import pandas as pd
+
+ticker_list = [
+                   'C',
+                   'W',
+                   'KW',
+                    'S',
+                    'SM',
+                    'BO',
+                    'LC',
+                    'LH',
+                   'HG',
+                   'CL',
+                   'CO',
+                   'GO',
+                    'HO',
+                    'XB',
+                   'NG',
+                   'SB',
+                   'CC',
+                   'KC'
+                   ]
+
+
+DATA_DIR = 'X:\data\continuous_time_series'
+
+file_suffix = '_flat_price_on_fnd_continuous.feather'
+
+ts_ = []
+for ticker in ticker_list:
+    file_name = f'{ticker}{file_suffix}'
+    file_path = os.path.join(DATA_DIR, file_name)
+    df_ = pd.read_feather(file_path)
+    ts = (df_.set_index('datetime')['dod_change']/df_.set_index('datetime')['close']).to_frame(ticker)
+    ts_.append(ts)
+
+df0 = pd.concat(ts_, axis=1)['2004':]
+
+print(df0)
